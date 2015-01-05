86a68b5b5bbac67faa6fb65db9079614288ebe3c
Jacky Lin <online.et@gmail.com>
2015-01-05 14:14:45 +0800

    Now not found query will return id with 0 in json format.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,7 +11,7 @@ class UsersController < ApplicationController
       if !@user.blank?
         redirect_to user_path(@user, format: :json)
       else
-        render :json => { :errors => "There's not user with specified email."}
+        render :json => { :id => "0"}
       end
     end
   end
@@ -38,7 +38,7 @@ class UsersController < ApplicationController
     respond_to do |format|
       if @user.save
         format.html { redirect_to @user, notice: 'User was successfully created.' }
-        format.json { render :show, status: :created, location: @user }
+        render.json { render :show, status: :created, location: @user }
       else
         format.html { render :new }
         format.json { render json: @user.errors, status: :unprocessable_entity }
