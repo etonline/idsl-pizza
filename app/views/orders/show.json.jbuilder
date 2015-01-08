5a8e945d74342b9df43225335f10938164253272
Jacky Lin <online.et@gmail.com>
2015-01-08 13:13:21 +0800

    Add price and name for product under order_detail array in order#show json format.

--- app/views/orders/show.json.jbuilder
+++ app/views/orders/show.json.jbuilder
@@ -1,4 +1,7 @@
 json.(@order, :id, :deliver_time, :total_price, :pay_type_id, :contact_phone, :deliver_address, :status, :created_at, :bonus, :order_time, :updated_at)
 json.order_details(@order_details) do |order_detail|
-  json.(order_detail, :id, :order_id, :product_id, :quantity, :created_at, :updated_at)
+  json.(order_detail, :id, :order_id, :product_id,)
+  json.product_name order_detail.product.name
+  json.product_price order_detail.product.price
+  json.(order_detail, :quantity, :created_at, :updated_at)
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150106131550) do
+ActiveRecord::Schema.define(version: 20150108045658) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "name"
@@ -39,6 +39,7 @@ ActiveRecord::Schema.define(version: 20150106131550) do
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
     t.string   "status"
+    t.datetime "order_time"
   end
 
   create_table "products", force: :cascade do |t|
